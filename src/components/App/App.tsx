c6301509cce28782ddc3947be2de7420479723a4
Paolo Dapul <paolodapultech@gmail.com>
2022-10-31 20:11:26 +0800

    Setup SVG importing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import { ErrorFallback } from "@components/ErrorFallback";
 import { ErrorBoundary } from "react-error-boundary";
+import { ReactComponent as Icon } from "@assets/icon.svg";
 
 function App() {
   return (
     <div>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <div className="text-2xl">React + Vite + TypeScript starter</div>
+        <div className="text-2xl">
+          <Icon className="w-[20px] h-[20px] inline mr-4" />
+          React + Vite + TypeScript starter
+        </div>
       </ErrorBoundary>
     </div>
   );
